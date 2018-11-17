752fb0d0b8d7495ca0b6d02cc931755534f9b6e5
Pom2Terre <pomme.deterre9@gmail.com>
2018-11-17 17:59:43 +0100

    Test POST /todos route + server-test.js added

---
+++ server/test/server-test.js
@@ -0,0 +1,57 @@
+const supertest = require('supertest');
+const expect = require('expect');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+beforeEach((done) => {  // will run before every single test case
+  Todo.deleteMany({}).then (() => { // delete all the TodaApp content!
+    done();
+  })
+});
+
+describe('POST /todos', function() {
+  it('should create a new todo', (done) => {
+    var text = 'Test todo text document';
+
+    supertest(app)
+      .post('/todos')
+      .send({text})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err); // generate an error message and exit the test
+        }
+        Todo.find() // if OK, we check the response in mongodb
+        .then((todos) => { //load all the todos
+          expect(todos.length).toBe(1); //only one record found see the beforeEach section
+          expect(todos[0].text).toBe(text); // db text === text variable
+          done();
+        })
+        .catch((err) => done(err));
+      });
+  });
+
+  it('should NOT create a todo with invalid body data', (done) => {
+    // we do not pass any text at all => invalid data!
+    supertest(app)
+      .post('/todos')
+      .send({}) // send nothing
+      .expect(400)  // bad request
+      .end((err, res) => {
+        if (err) {
+          return done(res);
+        }
+      });
+
+      Todo.find()
+      .then((todos) => {
+        expect(todos.length).toBe(0);
+        done();
+      })
+      .catch((err) => done(err));
+  });
+});
