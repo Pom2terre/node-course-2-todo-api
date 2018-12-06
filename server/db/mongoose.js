3662f913064aec8d40e11b71518cce3df55fae35
pom2terre <pomme.deterre9@gmail.com>
2018-12-06 17:50:16 +0100

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // tells mongoose we want to use Promises
-mongoose.connect('mongodb://localhost:27017/TodaApp', {
-  useNewUrlParser: true
-});
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodaApp', {
+  useNewUrlParser: true});
 
-module.exports = {
-  mongoose
-};
+module.exports = {mongoose};
